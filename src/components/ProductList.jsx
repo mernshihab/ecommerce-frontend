b165c0fc28a0ecaf65579dae1d24e4436a7db546
MD Shihab <166078352+mernshihab@users.noreply.github.com>
2024-06-12 21:39:14 +0600

    Add loader

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,17 +2,42 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ProductCard from "./ProductCard";
 import Helper from "../utility/Helper";
+import { TailSpin } from "react-loader-spinner";
 
 const ProductList = ({ isCartOpen }) => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get(`${Helper.API_BASE}/products`)
-      .then((response) => setProducts(response.data))
-      .catch((error) => console.error(error));
+      .then((response) => {
+        setProducts(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
   }, []);
 
+  if (loading) {
+    return (
+      <div className="p-6 flex flex-col justify-center items-center w-full h-72">
+        <p className="font-bold text-xl mb-1">Please wait or reload again</p>
+        <TailSpin
+          visible={true}
+          height="60"
+          width="60"
+          color="#4fa94d"
+          ariaLabel="tail-spin-loading"
+          radius="1"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      </div>
+    );
+  }
 
   return (
     <div
